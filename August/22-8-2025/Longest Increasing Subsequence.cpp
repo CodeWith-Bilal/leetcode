73bd00fd7ac3ed6ed993742ed8207a65d0a432aa
CodeWith-Bilal <mb03086350364@gmail.com>
2025-01-01 10:00:23 +0500

    Add implementation for Longest Increasing Subsequence leetcode interview question

---
+++ August/22-8-2025/Longest Increasing Subsequence.cpp
@@ -0,0 +1,21 @@
+//leetcode interview question
+//Given an integer array nums, return the length of the longest strictly increasing subsequence.
+
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, 1); 
+        int lis = 1;
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            lis = max(lis, dp[i]);
+        }
+        return lis;
+    }
+};

38cb351a9ddc33a6b3f61ed2439068a4afe3e577
CodeWith-Bilal <mb03086350364@gmail.com>
2025-01-01 09:56:15 +0500

    Add implementation for Word Break leetcode interview question

---
+++ August/22-8-2025/Word Break.cpp
@@ -0,0 +1,24 @@
+//leetcode interview question
+//Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+// Note that the same word in the dictionary may be reused multiple times in the segmentation
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n + 1, false);
+        dp[0] = true; 
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dict.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+    }
+};

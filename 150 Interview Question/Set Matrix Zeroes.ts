2263e3702a3814608930a5ff676195f3cdc07bc4
CodeWithMian <mb03086350364@gmail.com>
2025-11-17 21:32:30 +0500

    Implement setZeroes function to set matrix rows and columns to zero based on zeroes in the input matrix

---
+++ 150 Interview Question/Set Matrix Zeroes.ts
@@ -0,0 +1,46 @@
+function setZeroes(matrix: number[][]): void {
+    let m = matrix.length, n = matrix[0].length;
+    let firstRowZero = false, firstColZero = false;
+    for (let j = 0; j < n; j++) {
+        if (matrix[0][j] === 0) {
+            firstRowZero = true;
+            break;
+        }
+    }
+
+    for (let i = 0; i < m; i++) {
+        if (matrix[i][0] === 0) {
+            firstColZero = true;
+            break;
+        }
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][j] === 0) {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0; 
+            }
+        }
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (matrix[i][0] === 0 || matrix[0][j] === 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+
+    if (firstRowZero) {
+        for (let j = 0; j < n; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (firstColZero) {
+        for (let i = 0; i < m; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+}

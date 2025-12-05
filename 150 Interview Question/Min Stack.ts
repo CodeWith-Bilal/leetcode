69b0879fa2ce1be8b3ed56825d22a300770ad683
CodeWithMian <mb03086350364@gmail.com>
2025-12-05 21:45:31 +0500

    Implement MinStack class to support stack operations with minimum retrieval

---
+++ 150 Interview Question/Min Stack.ts
@@ -0,0 +1,32 @@
+class MinStack {
+    private stack: number[];
+    private minStack: number[];
+
+    constructor() {
+        this.stack = [];
+        this.minStack = [];
+    }
+
+    push(val: number): void {
+        this.stack.push(val);
+
+        if (this.minStack.length === 0 || val <= this.getMin()) {
+            this.minStack.push(val);
+        } else {
+            this.minStack.push(this.getMin());
+        }
+    }
+
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}

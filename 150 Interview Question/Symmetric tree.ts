fe9dc859e31c73a8b7564c801da077750badfd17
CodeWithMian <mb03086350364@gmail.com>
2025-12-22 20:02:11 +0500

    Implement isSymmetric function to check if a binary tree is symmetric

---
+++ 150 Interview Question/Symmetric tree.ts
@@ -0,0 +1,15 @@
+function isSymmetric(root: TreeNode | null): boolean {
+    if (!root) return true;
+
+    function isMirror(t1: TreeNode | null, t2: TreeNode | null): boolean {
+        if (!t1 && !t2) return true;
+        if (!t1 || !t2) return false;
+        return (
+            t1.val === t2.val &&
+            isMirror(t1.left, t2.right) &&
+            isMirror(t1.right, t2.left)
+        );
+    }
+
+    return isMirror(root.left, root.right);
+}
